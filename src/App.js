40dfcf18778eae0e2672adc6539fe912ee03e5e2
zegiha <LeeSeoYul07081@gmail.com>
2023-12-06 21:12:35 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import MyDocument from "./Page/MyDocument";
+import {PDFViewer} from "@react-pdf/renderer";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Controllbar>
+        <FunctionalBtn>텍스트</FunctionalBtn>
+        <FunctionalBtn>이미지</FunctionalBtn>
+      </Controllbar>
+      <PdfContainer>
+        <PDFViewer style={{width: '100%', height:'100%'}}>
+          <MyDocument/>
+        </PDFViewer>
+        <InputCotainer />
+      </PdfContainer>
+    </Container>
   );
 }
+
+const FunctionalBtn = styled.button`
+  width: 100px;
+  height: 100%;
+`;
+const InputCotainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 0, 0, 0.5);
+`;
+const Controllbar = styled.div`
+  width: 45%;
+  height: 6vh;
+  background-color: blue;
+  display: flex;
+  gap: 20px;
+`;
+const PdfContainer = styled.div`
+  position: relative;
+  width: 45%;
+  height: 93vh;
+`;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
 
 export default App;
